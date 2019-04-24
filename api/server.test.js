88c4a69814944066f69c426d13646fc3ce92fc2c
Trip4077 <bj2336@email.vccs.edu>
2019-04-24 00:03:29 -0400

    Server GET / test 1 FAIL

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const server = require('./server');
 
-const db = require('../dbConfig');
+const db = require('../data/dbConfig');
 
 describe('SERVER', () => {
     describe('Environment', () => {
@@ -9,6 +9,14 @@ describe('SERVER', () => {
             const env = process.env.DB_CONNECT;
 
             expect(env).toBe('testing');
+        });
+    });
+
+    describe('GET /', () => {
+        it('should return { api: "Up and Running" }', () => {
+            const res = request(server).get('/');
+
+            expect(res.body).toEqual({ api: 'Up and Running' });
         });
     });
 });
