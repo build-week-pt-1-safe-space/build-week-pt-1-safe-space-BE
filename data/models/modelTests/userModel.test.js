018e39b07ed69cc2d4e1c46c9ca9062db17f48bb
Trip4077 <bj2336@email.vccs.edu>
2019-04-24 00:35:47 -0400

    getAllUsers test 2 PASS

--- data/models/modelTests/userModel.test.js
+++ data/models/modelTests/userModel.test.js
@@ -10,8 +10,15 @@ describe('Users Model', () => {
     describe('getAllUsers()', () => {
         it('should return an array', async () => {
             const users = await Users.getAllUsers();
-        
+     
             expect(Array.isArray(users)).toBe(true);
+        });
+
+        it('should return an empty array if no users found', async () => {
+            const users = await Users.getAllUsers();
+
+            expect(users).toEqual([]);
+            expect(users.length).toBe(0);
         });
     });
 });
