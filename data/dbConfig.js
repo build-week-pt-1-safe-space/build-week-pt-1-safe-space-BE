e305c27ac1b00fec58713e2afd3c1072af22609b
Trip4077 <bj2336@email.vccs.edu>
2019-04-25 22:52:08 -0400

    DB Config Updates

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,9 +1,7 @@
-require('dotenv').config();
-
 const knex = require('knex');
-const config = require('../knexfile');
+const config = require('../knexfile')[dbEnv];
 
 //Testing or Developement Environment
 const dbEnv = process.env.DB_CONNECT || 'development';
 
-module.exports = knex(config[dbEnv]);
+module.exports = knex(config);
