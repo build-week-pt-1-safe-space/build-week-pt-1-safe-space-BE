b65341531fb115cbe35e33e8dfba2d8262c23160
Randy Wilson <phoenixrr2113@gmail.com>
2019-04-25 23:27:19 -0400

    added production env

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,42 @@
 module.exports = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './data/db.sqlite3',
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './data/migrations',
+		},
+		seeds: {
+			directory: './data/seeds',
+		},
+	},
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/db.sqlite3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
-  },
+	production: {
+		client: 'pg',
+		connection: {
+			filename: './data/db.sqlite3',
+		},
+		migrations: {
+			directory: './data/migrations',
+		},
+		seeds: {
+			directory: './data/seeds',
+		},
+	},
 
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/test.db3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
-  },
+	testing: {
+		client: 'sqlite3',
+		connection: {
+			filename: './data/test.db3',
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './data/migrations',
+		},
+		seeds: {
+			directory: './data/seeds',
+		},
+	},
 };
