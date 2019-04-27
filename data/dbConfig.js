b9b1a782e8a1509b2b7e9b94893d72ffdc199544
Trip4077 <bj2336@email.vccs.edu>
2019-04-27 12:37:50 -0400

    Debugging

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,14 +1,6 @@
 const knex = require('knex');
-<<<<<<< HEAD
+//Testing or Developement Environment
 const dbEnv = process.env.DB_CONNECT || 'development';
-=======
->>>>>>> e305c27ac1b00fec58713e2afd3c1072af22609b
 const config = require('../knexfile')[dbEnv];
 
-//Testing or Developement Environment
-
-<<<<<<< HEAD
 module.exports = knex(config);
-=======
-module.exports = knex(config);
->>>>>>> e305c27ac1b00fec58713e2afd3c1072af22609b
